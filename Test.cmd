a24757b6793d61dfdf900bbbae9b85fc09fd5323
Tobias Koch <t.koch@tk-software.de>
2017-02-07 20:06:33 +0100

    Adding code coverage report output to test script

--- Test.cmd
+++ Test.cmd
@@ -19,6 +19,14 @@ echo Running unit tests
 .\packages\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console.exe .\Yakka\bin\Debug\Yakka.Test\Yakka.test.dll --work=.\Yakka\bin\Debug\Yakka.Test\ --result=Yakka.Test.xml
 if errorlevel 1 goto error
 
+echo Running code coverage analysis
+.\packages\OpenCover.4.6.519\tools\OpenCover.Console.exe -register:user -target:.\packages\NUnit.ConsoleRunner.3.6.0\tools\nunit3-console.exe "-targetargs:.\Yakka\bin\Debug\Yakka.Test\Yakka.test.dll --result=Yakka.Test.xml" -output:.\Yakka\bin\Debug\Yakka.Test\Yakka.Coverage.xml
+if errorlevel 1 goto error
+
+echo Generating coverage report
+packages\ReportGenerator.2.5.2\tools\ReportGenerator.exe -reports:.\Yakka\bin\Debug\Yakka.Test\Yakka.Coverage.xml -targetdir:.\Yakka\bin\Debug\Yakka.Test\Coverage-Report
+if errorlevel 1 goto error
+
 :success
 echo.
 echo Test successful
