f5d991041c4a7c8887d759641dfd6b09f4d02ae4
Tobias Koch <t.koch@tk-software.de>
2017-02-10 20:45:26 +0100

    Adding new label class

---
+++ Yakka/Yakka/Utility/SmoothLabel.cs
@@ -0,0 +1,45 @@
+﻿#region GNU General Public License 3
+//  Yakka - A system tray application calculating and displaying your working hours
+//  Copyright (C) 2017  Tobias Koch
+//
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with this program. If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+#region Namespaces
+using System.Drawing.Text;
+using System.Windows.Forms;
+#endregion
+
+namespace Yakka.Utility
+{
+    /// <summary>
+    /// Represents a <see cref="Label"/> with antialiasing enabled.
+    /// </summary>
+    public class SmoothLabel : Label
+    {
+        #region Methods
+
+        /// <summary>
+        /// Raises the <see cref="Control.Paint"/> event.
+        /// </summary>
+        /// <param name="e">A <see cref="PaintEventArgs"/> that contains the event data.</param>
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            e.Graphics.TextRenderingHint = TextRenderingHint.AntiAliasGridFit;
+            base.OnPaint(e);
+        }
+
+        #endregion
+    }
+}
