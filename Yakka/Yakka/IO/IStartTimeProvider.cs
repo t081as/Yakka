147de8344e1901d7eb2cd5bea4d00e5cf42603ea
Tobias Koch <t.koch@tk-software.de>
2017-03-10 23:41:13 +0100

    Adding start time provider infrastructure

---
+++ Yakka/Yakka/IO/IStartTimeProvider.cs
@@ -0,0 +1,42 @@
+﻿#region GNU General Public License 3
+// Yakka - A system tray application calculating and displaying your working hours
+// Copyright (C) 2017  Tobias Koch
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+#endregion
+
+#region Namespaces
+using System;
+#endregion
+
+namespace Yakka.IO
+{
+    /// <summary>
+    /// Provides the ability to determine the start time of the working day.
+    /// </summary>
+    public interface IStartTimeProvider
+    {
+        #region Properties
+
+        /// <summary>
+        /// Gets the start time of the working day.
+        /// </summary>
+        DateTime? StartTime
+        {
+            get;
+        }
+
+        #endregion
+    }
+}
