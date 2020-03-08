9b7ca9a2d0ba263182fb93b2563b92c232477392
Tobias Koch <t.koch@tk-software.de>
2020-03-08 12:59:21 +0100

    Adding working hours calculator

--- src/Yakka/Calculation/IWorkingHoursCalculator.cs
+++ src/Yakka/Calculation/IWorkingHoursCalculator.cs
@@ -53,6 +53,7 @@ namespace Yakka.Calculation
         /// <item>breakTimeSpan: a <see cref="TimeSpan"/> representing the calculated break.</item>
         /// </list>
         /// </returns>
+        /// <exception cref="ArgumentException"><c>startTime</c> is greater than <c>endTime</c>.</exception>
         (TimeSpan workTimeSpan, TimeSpan breakTimeSpan) Calculate(DateTime startTime, DateTime endTime);
     }
 }
