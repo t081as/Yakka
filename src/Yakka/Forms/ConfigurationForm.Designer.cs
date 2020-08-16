5cb1f970904aa19f0b6b83dafb4f59069034d21a
Tobias Koch <t.koch@tk-software.de>
2020-08-16 21:10:05 +0200

    Adding gui elements

--- src/Yakka/Forms/ConfigurationForm.Designer.cs
+++ src/Yakka/Forms/ConfigurationForm.Designer.cs
@@ -44,21 +44,130 @@ namespace Yakka.Forms
         /// </summary>
         private void InitializeComponent()
         {
+            this.labelStartTime = new System.Windows.Forms.Label();
+            this.dateTimeStartTime = new System.Windows.Forms.DateTimePicker();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.labelCalculator = new System.Windows.Forms.Label();
+            this.checkBoxManual = new System.Windows.Forms.CheckBox();
+            this.maskedTextBoxManualBreak = new System.Windows.Forms.MaskedTextBox();
+            this.buttonCancel = new System.Windows.Forms.Button();
+            this.buttonApply = new System.Windows.Forms.Button();
+            this.buttonConfirm = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // labelStartTime
+            // 
+            this.labelStartTime.AutoSize = true;
+            this.labelStartTime.Location = new System.Drawing.Point(12, 18);
+            this.labelStartTime.Name = "labelStartTime";
+            this.labelStartTime.Size = new System.Drawing.Size(67, 15);
+            this.labelStartTime.TabIndex = 0;
+            this.labelStartTime.Text = "START TIME";
+            // 
+            // dateTimeStartTime
+            // 
+            this.dateTimeStartTime.Format = System.Windows.Forms.DateTimePickerFormat.Time;
+            this.dateTimeStartTime.Location = new System.Drawing.Point(148, 12);
+            this.dateTimeStartTime.Name = "dateTimeStartTime";
+            this.dateTimeStartTime.Size = new System.Drawing.Size(300, 23);
+            this.dateTimeStartTime.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(148, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(300, 23);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // labelCalculator
+            // 
+            this.labelCalculator.AutoSize = true;
+            this.labelCalculator.Location = new System.Drawing.Point(12, 44);
+            this.labelCalculator.Name = "labelCalculator";
+            this.labelCalculator.Size = new System.Drawing.Size(79, 15);
+            this.labelCalculator.TabIndex = 0;
+            this.labelCalculator.Text = "CALCULATOR";
+            // 
+            // checkBoxManual
+            // 
+            this.checkBoxManual.AutoSize = true;
+            this.checkBoxManual.Location = new System.Drawing.Point(12, 72);
+            this.checkBoxManual.Name = "checkBoxManual";
+            this.checkBoxManual.Size = new System.Drawing.Size(114, 19);
+            this.checkBoxManual.TabIndex = 3;
+            this.checkBoxManual.Text = "MANUAL BREAK";
+            this.checkBoxManual.UseVisualStyleBackColor = true;
+            // 
+            // maskedTextBoxManualBreak
+            // 
+            this.maskedTextBoxManualBreak.Location = new System.Drawing.Point(148, 70);
+            this.maskedTextBoxManualBreak.Mask = "90:00";
+            this.maskedTextBoxManualBreak.Name = "maskedTextBoxManualBreak";
+            this.maskedTextBoxManualBreak.Size = new System.Drawing.Size(300, 23);
+            this.maskedTextBoxManualBreak.TabIndex = 4;
+            // 
+            // buttonCancel
+            // 
+            this.buttonCancel.Location = new System.Drawing.Point(373, 99);
+            this.buttonCancel.Name = "buttonCancel";
+            this.buttonCancel.Size = new System.Drawing.Size(75, 23);
+            this.buttonCancel.TabIndex = 5;
+            this.buttonCancel.Text = "CANCEL";
+            this.buttonCancel.UseVisualStyleBackColor = true;
+            // 
+            // buttonApply
+            // 
+            this.buttonApply.Location = new System.Drawing.Point(292, 99);
+            this.buttonApply.Name = "buttonApply";
+            this.buttonApply.Size = new System.Drawing.Size(75, 23);
+            this.buttonApply.TabIndex = 5;
+            this.buttonApply.Text = "APPLY";
+            this.buttonApply.UseVisualStyleBackColor = true;
+            // 
+            // buttonConfirm
+            // 
+            this.buttonConfirm.Location = new System.Drawing.Point(211, 99);
+            this.buttonConfirm.Name = "buttonConfirm";
+            this.buttonConfirm.Size = new System.Drawing.Size(75, 23);
+            this.buttonConfirm.TabIndex = 5;
+            this.buttonConfirm.Text = "OK";
+            this.buttonConfirm.UseVisualStyleBackColor = true;
             // 
             // ConfigurationForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(530, 328);
-            this.MinimizeBox = false;
+            this.ClientSize = new System.Drawing.Size(460, 134);
+            this.Controls.Add(this.buttonConfirm);
+            this.Controls.Add(this.buttonApply);
+            this.Controls.Add(this.buttonCancel);
+            this.Controls.Add(this.maskedTextBoxManualBreak);
+            this.Controls.Add(this.checkBoxManual);
+            this.Controls.Add(this.labelCalculator);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dateTimeStartTime);
+            this.Controls.Add(this.labelStartTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
             this.Name = "ConfigurationForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "ConfigurationForm";
+            this.Text = "YAKKA CONFIGURATION";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label labelStartTime;
+        private System.Windows.Forms.DateTimePicker dateTimeStartTime;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label labelCalculator;
+        private System.Windows.Forms.CheckBox checkBoxManual;
+        private System.Windows.Forms.MaskedTextBox maskedTextBoxManualBreak;
+        private System.Windows.Forms.Button buttonCancel;
+        private System.Windows.Forms.Button buttonApply;
+        private System.Windows.Forms.Button buttonConfirm;
     }
 }
