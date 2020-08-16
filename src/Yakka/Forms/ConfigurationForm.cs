f2a3d1d6c609e159535f43fb837280020b6cee4d
Tobias Koch <t.koch@tk-software.de>
2020-08-16 17:58:22 +0200

    Adding configuration view

---
+++ src/Yakka/Forms/ConfigurationForm.cs
@@ -0,0 +1,60 @@
+﻿// Yakka - A system tray application calculating and displaying your working hours
+// Copyright (C) 2017-2020  Tobias Koch
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Windows.Forms;
+using Yakka.Calculation;
+
+namespace Yakka.Forms
+{
+    /// <summary>
+    /// Represents the window that allows the user to change the configuration used to calculate the working hours.
+    /// </summary>
+    public partial class ConfigurationForm : Form, IConfigurationView
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ConfigurationForm"/> class.
+        /// </summary>
+        /// <param name="availableCalculators">The available working hours calculatrors the user may choose from.</param>
+        public ConfigurationForm(IEnumerable<IWorkingHoursCalculator> availableCalculators)
+        {
+            if (availableCalculators == null)
+            {
+                throw new ArgumentNullException(nameof(availableCalculators));
+            }
+
+            this.InitializeComponent();
+            this.Icon = Icon.ExtractAssociatedIcon(Assembly.GetExecutingAssembly().Location);
+            this.WorkingHoursCalculator = availableCalculators.First();
+        }
+
+        /// <inheritdoc />
+        public DateTime StartTime { get; set; }
+
+        /// <inheritdoc />
+        public IWorkingHoursCalculator WorkingHoursCalculator { get; set; }
+
+        /// <inheritdoc />
+        public BreakMode BreakMode { get; set; }
+
+        /// <inheritdoc />
+        public TimeSpan ManualBreakTime { get; set; }
+    }
+}
---
+++ src/Yakka/Forms/IConfigurationView.cs
@@ -0,0 +1,58 @@
+﻿// Yakka - A system tray application calculating and displaying your working hours
+// Copyright (C) 2017-2020  Tobias Koch
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+using System;
+using Yakka.Calculation;
+
+namespace Yakka.Forms
+{
+    /// <summary>
+    /// Describes objects that serve as a view for changing the working hours configuration.
+    /// </summary>
+    public interface IConfigurationView
+    {
+        /// <summary>
+        /// Gets or sets the start date and time of the current working day.
+        /// </summary>
+        /// <value>The start date and time of the current working day.</value>
+        DateTime StartTime { get; set; }
+
+        /// <summary>
+        /// Gets or sets the implementation of the <see cref="IWorkingHoursCalculator"/> interface
+        /// that shall be used to calculate.
+        /// </summary>
+        /// <value>
+        /// The implementation of the <see cref="IWorkingHoursCalculator"/> interface
+        /// that shall be used to calculate.
+        /// </value>
+        IWorkingHoursCalculator WorkingHoursCalculator { get; set; }
+
+        /// <summary>
+        /// Gets or sets the break mode configured by the user.
+        /// </summary>
+        /// <value>The break mode configured by the user.</value>
+        BreakMode BreakMode { get; set; }
+
+        /// <summary>
+        /// Gets or sets the manual break configured by the user.
+        /// </summary>
+        /// <value>The manual break configured by the user.</value>
+        /// <remarks>
+        /// This value will only be used if <see cref="BreakMode"/> is set to <see cref="BreakMode.Manual"/>.
+        /// </remarks>
+        TimeSpan ManualBreakTime { get; set; }
+    }
+}
