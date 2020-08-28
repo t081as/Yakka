9f08189e4e5d5f4a914211d8168ca55501551828
Tobias Koch <t.koch@tk-software.de>
2020-08-28 21:38:30 +0200

    Adding view interface

--- src/Yakka/Forms/AboutForm.Designer.cs
+++ src/Yakka/Forms/AboutForm.Designer.cs
@@ -49,14 +49,20 @@ namespace Yakka.Forms
             this.pictureBoxLogo = new System.Windows.Forms.PictureBox();
             this.tabControl = new System.Windows.Forms.TabControl();
             this.tabPageInformation = new System.Windows.Forms.TabPage();
+            this.tableLayoutPanelInformation = new System.Windows.Forms.TableLayoutPanel();
+            this.labelDescription = new System.Windows.Forms.Label();
+            this.labelVersion = new System.Windows.Forms.Label();
             this.buttonClose = new System.Windows.Forms.Button();
+            this.labelName = new System.Windows.Forms.Label();
             this.tabPageAuthors = new System.Windows.Forms.TabPage();
             this.tabPageLicense = new System.Windows.Forms.TabPage();
             this.textBoxLicense = new System.Windows.Forms.TextBox();
+            this.labelCopyright = new System.Windows.Forms.Label();
             this.tableLayoutPanel.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).BeginInit();
             this.tabControl.SuspendLayout();
             this.tabPageInformation.SuspendLayout();
+            this.tableLayoutPanelInformation.SuspendLayout();
             this.tabPageLicense.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -102,7 +108,7 @@ namespace Yakka.Forms
             // tabPageInformation
             // 
             this.tabPageInformation.BackColor = System.Drawing.SystemColors.ControlLightLight;
-            this.tabPageInformation.Controls.Add(this.buttonClose);
+            this.tabPageInformation.Controls.Add(this.tableLayoutPanelInformation);
             this.tabPageInformation.Location = new System.Drawing.Point(4, 24);
             this.tabPageInformation.Name = "tabPageInformation";
             this.tabPageInformation.Padding = new System.Windows.Forms.Padding(3);
@@ -110,15 +116,69 @@ namespace Yakka.Forms
             this.tabPageInformation.TabIndex = 0;
             this.tabPageInformation.Text = "INFORMATION";
             // 
+            // tableLayoutPanelInformation
+            // 
+            this.tableLayoutPanelInformation.ColumnCount = 1;
+            this.tableLayoutPanelInformation.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanelInformation.Controls.Add(this.labelDescription, 0, 2);
+            this.tableLayoutPanelInformation.Controls.Add(this.labelVersion, 0, 1);
+            this.tableLayoutPanelInformation.Controls.Add(this.buttonClose, 0, 4);
+            this.tableLayoutPanelInformation.Controls.Add(this.labelName, 0, 0);
+            this.tableLayoutPanelInformation.Controls.Add(this.labelCopyright, 0, 3);
+            this.tableLayoutPanelInformation.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanelInformation.Location = new System.Drawing.Point(3, 3);
+            this.tableLayoutPanelInformation.Name = "tableLayoutPanelInformation";
+            this.tableLayoutPanelInformation.RowCount = 5;
+            this.tableLayoutPanelInformation.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelInformation.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelInformation.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelInformation.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelInformation.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
+            this.tableLayoutPanelInformation.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanelInformation.Size = new System.Drawing.Size(564, 245);
+            this.tableLayoutPanelInformation.TabIndex = 0;
+            // 
+            // labelDescription
+            // 
+            this.labelDescription.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelDescription.Location = new System.Drawing.Point(3, 98);
+            this.labelDescription.Name = "labelDescription";
+            this.labelDescription.Size = new System.Drawing.Size(558, 49);
+            this.labelDescription.TabIndex = 1;
+            this.labelDescription.Text = "DESCRIPTION";
+            this.labelDescription.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelVersion
+            // 
+            this.labelVersion.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelVersion.Font = new System.Drawing.Font("Segoe UI Semibold", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelVersion.Location = new System.Drawing.Point(3, 49);
+            this.labelVersion.Name = "labelVersion";
+            this.labelVersion.Size = new System.Drawing.Size(558, 49);
+            this.labelVersion.TabIndex = 1;
+            this.labelVersion.Text = "VERSION";
+            this.labelVersion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // buttonClose
             // 
             this.buttonClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonClose.Location = new System.Drawing.Point(492, 223);
+            this.buttonClose.Location = new System.Drawing.Point(486, 219);
             this.buttonClose.Name = "buttonClose";
             this.buttonClose.Size = new System.Drawing.Size(75, 23);
             this.buttonClose.TabIndex = 0;
             this.buttonClose.Text = "CLOSE";
             this.buttonClose.UseVisualStyleBackColor = true;
+            // 
+            // labelName
+            // 
+            this.labelName.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelName.Font = new System.Drawing.Font("Segoe UI Semibold", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labelName.Location = new System.Drawing.Point(3, 0);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(558, 49);
+            this.labelName.TabIndex = 1;
+            this.labelName.Text = "NAME";
+            this.labelName.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // tabPageAuthors
             // 
@@ -152,6 +212,16 @@ namespace Yakka.Forms
             this.textBoxLicense.Size = new System.Drawing.Size(570, 251);
             this.textBoxLicense.TabIndex = 0;
             // 
+            // labelCopyright
+            // 
+            this.labelCopyright.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelCopyright.Location = new System.Drawing.Point(3, 147);
+            this.labelCopyright.Name = "labelCopyright";
+            this.labelCopyright.Size = new System.Drawing.Size(558, 49);
+            this.labelCopyright.TabIndex = 2;
+            this.labelCopyright.Text = "COPYRIGHT";
+            this.labelCopyright.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
@@ -166,6 +236,7 @@ namespace Yakka.Forms
             ((System.ComponentModel.ISupportInitialize)(this.pictureBoxLogo)).EndInit();
             this.tabControl.ResumeLayout(false);
             this.tabPageInformation.ResumeLayout(false);
+            this.tableLayoutPanelInformation.ResumeLayout(false);
             this.tabPageLicense.ResumeLayout(false);
             this.tabPageLicense.PerformLayout();
             this.ResumeLayout(false);
@@ -180,7 +251,12 @@ namespace Yakka.Forms
         private System.Windows.Forms.TabPage tabPageInformation;
         private System.Windows.Forms.TabPage tabPageAuthors;
         private System.Windows.Forms.TabPage tabPageLicense;
-        private System.Windows.Forms.Button buttonClose;
         private System.Windows.Forms.TextBox textBoxLicense;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanelInformation;
+        private System.Windows.Forms.Label labelDescription;
+        private System.Windows.Forms.Label labelVersion;
+        private System.Windows.Forms.Button buttonClose;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelCopyright;
     }
 }
